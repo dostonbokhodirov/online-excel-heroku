41a8d54043b34eebb5690697152d255032000f4b
dostonbokhodirov <boxodirov.doston@gmail.com>
2022-03-25 17:58:12 +0500

    updated ExcelFileController

--- src/main/java/uz/excel/onlineexcel/OnlineExcelApplication.java
+++ src/main/java/uz/excel/onlineexcel/OnlineExcelApplication.java
@@ -1,15 +1,11 @@
 package uz.excel.onlineexcel;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import uz.excel.onlineexcel.dto.auth.AuthUserCreateDto;
 import uz.excel.onlineexcel.property.OpenApiProperties;
 import uz.excel.onlineexcel.property.ServerProperties;
-import uz.excel.onlineexcel.service.AuthService;
 
 
 @SpringBootApplication
@@ -22,28 +18,8 @@ import uz.excel.onlineexcel.service.AuthService;
 @OpenAPIDefinition
 public class OnlineExcelApplication {
 
-    @Autowired
-    static
-    AuthService userService;
-    @Autowired
-    static
-    PasswordEncoder passwordEncoder;
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineExcelApplication.class, args);
-    }
-
-//    @Bean
-//    CommandLineRunner run() {
-//        return args -> {
-//            userService.create(new AuthUserCreateDto("123", passwordEncoder.encode("123"),
-//                    "Saydali", "+998973130080", "Saydali@gmail.com"));
-//        };
-//    }
-
-    static {
-        userService.create(new AuthUserCreateDto("123", passwordEncoder.encode("123"),
-                "Saydali", "+998973130080", "Saydali@gmail.com"));
     }
 
 }
